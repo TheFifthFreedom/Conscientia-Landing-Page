21ca0225f66a4356b2d9e4e2f03378f37789df76
Ruriko Araki <ruriko.araki@gmail.com>
2014-02-11 15:57:56 -0500

    Script template for insert into DB

---
+++ insert.php
@@ -0,0 +1,22 @@
+<?php
+
+// $con=mysqli_connect("example.com","peter","abc123","my_db");
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+$sql="INSERT INTO Emails (email)
+VALUES
+('$_POST[email]')";
+
+if (!mysqli_query($con,$sql))
+  {
+  die('Error: ' . mysqli_error($con));
+  }
+echo "1 record added";
+
+mysqli_close($con);
+
+?>
--- sendresults.php
+++ sendresults.php
@@ -1,5 +1,4 @@
 <?php
-//--------------------------Set these paramaters--------------------------
 
 // Subject of email sent to you.
 $subject = 'Results from Contact form'; 
@@ -7,14 +6,28 @@ $subject = 'Results from Contact form';
 // Your email address. This is where the form information will be sent. 
 $emailadd = 'nanaryuu@gmail.com'; 
 
-// Where to redirect after form is processed. 
-// $url = 'http://www.columbia.edu/~lm2773/confirmation.html';
-
-// --------------------------Do not edit below this line--------------------------
 $space = ' ';
 $email = $HTTP_POST_VARS['email'];
-error_log( $email )
+debug_to_console( $email );
 $text = "New subscriber:$space{$email}";
-if (mail($emailadd, $subject, $text, 'From: Conscientia'))
-{error_log("Success")};
+if (mail($emailadd, $subject, $text)) {
+	debug_to_console("Success");
+} else {
+	debug_to_console("FAIL");
+}
+
+?>
+
+<?php
+/**
+ * Send debug code to the Javascript console
+ */ 
+function debug_to_console($data) {
+    if(is_array($data) || is_object($data))
+	{
+		echo("<script>console.log('PHP: ".json_encode($data)."');</script>");
+	} else {
+		echo("<script>console.log('PHP: ".$data."');</script>");
+	}
+}
 ?>
