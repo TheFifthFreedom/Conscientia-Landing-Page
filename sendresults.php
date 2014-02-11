d2d331899f45fa1764f54c78b422976792205b11
Ruriko Araki <ruriko.araki@gmail.com>
2014-02-11 01:59:14 -0500

    PHP file fixed maybe

--- sendresults.php
+++ sendresults.php
@@ -8,33 +8,20 @@ $subject = 'Results from Contact form';
 $emailadd = 'nanaryuu@gmail.com'; 
 
 // Where to redirect after form is processed. 
-// $url = 'http://www.columbia.edu/~lm2773/confirmation.html'; 
-
-// Makes all fields required. If set to '1' no field can not be empty. If set to '0' any or all fields can be empty.
-$req = '1'; 
+// $url = 'http://www.columbia.edu/~lm2773/confirmation.html';
 
 // --------------------------Do not edit below this line--------------------------
 $text = "Results from form:\n\n"; 
 $space = ' ';
-$line = '
-';
-foreach ($_POST as $key => $value)
-{
-if ($req == '1')
-{
-if ($value == '')
-{echo "$key is empty";die;}
-}
+$email = $HTTP_POST_VARS['email'];
 $j = strlen($key);
 if ($j >= 50)
 {echo "Name of form element $key cannot be longer than 50 characters";die;}
 $j = 50 - $j;
 for ($i = 1; $i <= $j; $i++)
 {$space .= ' ';}
-$value = str_replace('\n', "$line", $value);
-$conc = "{$key}:$space{$value}$line";
+$conc = "New subscriber:$space{$email}";
 $text .= $conc;
 $space = ' ';
-}
 mail($emailadd, $subject, $text, 'From: '.$emailadd.'');
 ?>
