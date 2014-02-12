97495bef7efc36b3aaf9d5b567a150118a02993e
Ruriko Araki <ruriko.araki@gmail.com>
2014-02-12 00:57:56 -0500

    HAH MADE THE FORM WORK

--- notify.php
+++ notify.php
@@ -1,35 +1,33 @@
 <?php
 
-// // $con=mysqli_connect("example.com","peter","abc123","my_db");
-// // Check connection
-// if (mysqli_connect_errno())
-//   {
-//   echo "Failed to connect to MySQL: " . mysqli_connect_error();
-//   }
+$dbhost = 'localhost:3306';
+$dbuser = 'laurent';
+$dbpass = 'meteora100';
+$conn = mysql_connect($dbhost, $dbuser, $dbpass);
 
-// $sql="INSERT INTO Emails (email)
-// VALUES
-// ('$_POST[email]')";
+if(! $conn )
+{
+  die('Could not connect: ' . mysql_error());
+}
 
-// if (!mysqli_query($con,$sql))
-//   {
-//   die('Error: ' . mysqli_error($con));
-//   }
-// echo "1 record added";
+mysql_select_db('Conscientia');
 
-// mysqli_close($con);
-
-$dbhost = "localhost";
-$dbuser = "TheSixthLiberty";
-$dbpass = "orfpWNasYWCP4FcLEFRkfvv4qANqui";
-$dbname = "Conscientia";
-$c = @mysql_pconnect($dbhost,$dbuser,$dbpass) or die();
-@mysql_select_db($dbname,$c) or die();
-
-//It isn't secure in this state: validation needed as it is an email?
 if(isset($_POST['email'])){
-    $email = mysql_real_escape_string($_POST['email']);
-    mysql_query('INSERT INTO emails (email) VALUES ('.$email.');');
+	$email = mysql_real_escape_string($_POST['email']);
+
+	$sql = 'INSERT INTO emails '.
+       '(email) '.
+       'VALUES ( "'.$email.'" )';
+    
+    $retval = mysql_query( $sql, $conn );
+    if(! $retval )
+    {
+    	die('Could not enter data: ' . mysql_error());
+    }
+    echo "Entered data successfully\n";
+	//mysql_close($conn);
+
+    //mysql_query('INSERT INTO emails (email) VALUES ('.$email.');');
 }
 
 ?>
